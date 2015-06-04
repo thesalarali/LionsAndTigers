54944a472870c1dfb75c697044c93da84dbd836f
Salar Ali <BAY50@Salars-MacBook-Pro.local>
2015-06-04 18:18:04 -0400

    second commit

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    
+    //----------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +29,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //----------------------------------------------
 
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+        
+        
+    }
 
 }
 
